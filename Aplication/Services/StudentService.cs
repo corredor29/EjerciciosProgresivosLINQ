c08856341544e006fcab0c9639814f141c9b3e01
corredor29 <197847818+corredor29@users.noreply.github.com>
2026-04-04 18:41:59 -0500

    feat: :sparkles: nivel 2 nombre y curso de cada estudiante

--- Aplication/Services/StudentService.cs
+++ Aplication/Services/StudentService.cs
@@ -11,11 +11,21 @@ public class StudentService
 {
     public void ShowStudents()
     {
+        // nivel 1 Filtrar estudiantes aprobados
+        Console.WriteLine("Filtrar estudiantes aprobados");
         var students = StudentData.GetStudents();
         var aprobados = students.Where(alumnos => alumnos.Nota >= 3.0);
         foreach (var alumnos in aprobados)
         {
             Console.WriteLine($"{alumnos.Nombre} - {alumnos.Nota}");
         }
+        // Nivel 2 nombre y curso de cada estudiante
+        Console.WriteLine("nombre y curso de cada estudiante");
+        var nameStudent = students.Select(name => $"{name.Nombre} - {name.Curso}");
+        foreach ( var name in nameStudent)
+        {
+            Console.WriteLine(name);
+        }
     }
+
 }
